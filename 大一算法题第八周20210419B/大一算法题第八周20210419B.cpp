bef28d895e7dcb95b63db29397f1f1bff6b54b3d
huicong2002 <1532751400@qq.com>
2021-04-22 19:47:54 +0800

    logs

---
+++ 大一算法题第八周20210419B/大一算法题第八周20210419B.cpp
@@ -0,0 +1,40 @@
+﻿//自然数拆分
+/*
+对于任意大于1的自然数n，总是可以拆分成若干个小于n的自然数之和。现请你编写程序求出n的所有拆分。*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int arr[25];
+int n;
+void dfs(int sum, int length, int start)
+{
+	if (sum == n)
+	{
+		printf("%d=%d", n, arr[0]);
+
+		for (int i = 1; i < length; i++)
+		{
+			printf("+%d", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+
+	for (int i = 1; i < n; i++)
+	{
+		if ((i + sum) < n && i >= start)
+		{
+			arr[length] = i;
+			length++;
+			dfs(sum + i, length, i);
+			length--;
+		}
+	}
+}
+int main()
+{
+	scanf("%d", &n);
+
+	dfs(0, 0, 1);
+
+	return 0;
+}
